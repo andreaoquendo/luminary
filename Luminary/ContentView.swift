3fb749d3ba513ef9eb063b3ed55f66fec89472d1
andreaoquendo <oquendo.3455@gmail.com>
2023-09-25 12:49:56 -0300

    feat: Add CoreData

--- Luminary/ContentView.swift
+++ Luminary/ContentView.swift
@@ -10,20 +10,22 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+    
+    @FetchRequest(sortDescriptors: [
+        NSSortDescriptor(keyPath: \Quote.quote, ascending: true)],
         animation: .default)
-    private var items: FetchedResults<Item>
+    var quotes: FetchedResults<Quote>
+//    let item: Item
+    
 
     var body: some View {
         NavigationView {
             List {
-                ForEach(items) { item in
+                ForEach(quotes) { quote in
                     NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                        Text(quote.quote ?? "placeholder")
                     } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+                        Text(quote.quote ?? "placeholder")
                     }
                 }
                 .onDelete(perform: deleteItems)
@@ -43,9 +45,11 @@ struct ContentView: View {
     }
 
     private func addItem() {
+        print("add")
         withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
+            let newItem = Quote(context: viewContext)
+            newItem.quote = "Novo"
+            newItem.date = Date()
 
             do {
                 try viewContext.save()
@@ -59,27 +63,21 @@ struct ContentView: View {
     }
 
     private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        print("delete")
+//        withAnimation {
+//            offsets.map { items[$0] }.forEach(viewContext.delete)
+//
+//            do {
+//                try viewContext.save()
+//            } catch {
+//                // Replace this implementation with code to handle the error appropriately.
+//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//                let nsError = error as NSError
+//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+//            }
+//        }
     }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
